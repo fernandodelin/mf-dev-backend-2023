226ff5dea4bccf20659af45202989766ea8239df
Fernando Anisio <fernandodevlinux@gmail.com>
2024-03-05 07:31:49 -0300

    Criação Controller Veiculos

---
+++ mf-dev-backend-2023/Controllers/VeiculosController.cs
@@ -0,0 +1,24 @@
+﻿using mf_dev_backend_2023.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace mf_dev_backend_2023.Controllers
+{
+
+    public class VeiculosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public VeiculosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var dados = await _context.Veiculos.ToListAsync();
+
+            return View(dados);
+        }
+    }
+}
